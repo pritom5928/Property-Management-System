b0b41ac63df640d3bbecb69ac17778410238e751
pritom5928 <paul15-5928@diu.edu.bd>
2022-04-24 21:36:28 +0600

    add custom middleware, generic error response and exception handle both built in and custom way

--- backend/HSPA/Middlewares/ExceptionMiddleware.cs
+++ backend/HSPA/Middlewares/ExceptionMiddleware.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HSPA.Errors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace HSPA.Middlewares
@@ -11,11 +15,13 @@ namespace HSPA.Middlewares
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IWebHostEnvironment env)
         {
             this.next = next;
             _logger = logger;
+            _env = env;
         }
 
         public async Task Invoke(HttpContext context)
@@ -26,9 +32,40 @@ namespace HSPA.Middlewares
             }
             catch (Exception ex)
             {
+                ApiError response;
+                HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+
+                string message;
+
+                var exceptionType = ex.GetType();
+
+                if (exceptionType == typeof(UnauthorizedAccessException))
+                {
+                    statusCode = HttpStatusCode.Forbidden;
+                    message = "You are not authorized!";
+                }
+                else
+                {
+                    statusCode = HttpStatusCode.InternalServerError;
+                    message = "Some unknown error occured!";
+                }
+
+
+
+                if (_env.IsDevelopment())
+                {
+                    response = new ApiError((int)statusCode, ex.Message, ex.StackTrace.ToString());
+                }
+                else
+                {
+                    response = new ApiError((int)statusCode, message);
+                }
+
                 _logger.LogError(ex.Message);
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(ex.Message);
+                context.Response.StatusCode = (int)statusCode;
+                context.Response.ContentType = "application/json";
+                
+                await context.Response.WriteAsync(response.ToString());
             }
         }
 
